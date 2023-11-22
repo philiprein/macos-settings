74159dec700eb15b18db1f4ef4e9538624b358e2
Philip Reinhart <74872363+philiprein@users.noreply.github.com>
2023-11-22 15:10:10 +0100

    update bluetooth settings

--- system_settings/bluetooth.sh
+++ system_settings/bluetooth.sh
@@ -4,18 +4,19 @@ echo "Bluetooth settings..."
 
 ### advanced...
 
-# open bluetooth assistant on boot if no keyboard is detected
+# open bluetooth assistant on boot if no keyboard is detected (default: on)
 # doesn't work permanently; setting will be reset when pref pane in system settins is opened
 # TODO
 # sudo defaults write /Library/Preferences/com.apple.Bluetooth.plist BluetoothAutoSeekKeyboard -bool false
 
-# open bluetooth assistant on boot if no mouse or trackpad is detected
+# open bluetooth assistant on boot if no mouse or trackpad is detected (default: on)
 # doesn't work permanently; setting will be reset when pref pane in system settins is opened
 # TODO
 # sudo defaults write /Library/Preferences/com.apple.Bluetooth.plist BluetoothAutoSeekPointingDevice -bool false
 
-
 ### hidden bluetooth tweaks
 
+# settings that are commented out are out of date or couldn't be validated to see if they were out of date or not
+
 # allow bluetooth devices to wake up the mac
-defaults -currentHost write com.apple.bluetooth RemoteWakeEnabled -boolean true
+# defaults -currentHost write com.apple.bluetooth RemoteWakeEnabled -boolean true
