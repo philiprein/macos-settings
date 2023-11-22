96fcf4432243fa2b74d432d083c082ba8fe9683c
Philip Reinhart <74872363+philiprein@users.noreply.github.com>
2023-11-22 15:11:27 +0100

    update screen time settings

--- system_settings/screen_time.sh
+++ system_settings/screen_time.sh
@@ -2,8 +2,4 @@
 
 echo "Screen Time settings..."
 
-# enable (needs logout)
-#defaults write com.apple.ScreenTimeAgent ScreenTimeEnabled -bool true
-
-# disbale (needs logout)
-defaults write com.apple.ScreenTimeAgent ScreenTimeEnabled -bool false
+# TODO
