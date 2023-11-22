f2fd2fc9a14f84dfa3a32bbd2bd790ddb152bf5b
Philip Reinhart <74872363+philiprein@users.noreply.github.com>
2023-11-22 15:11:35 +0100

    update sound settings

--- system_settings/sound.sh
+++ system_settings/sound.sh
@@ -2,34 +2,51 @@
 
 echo "Sound settings..."
 
-# disable sound effect on boot
-# %00 = on, %01 = off
-sudo nvram StartupMute=%01
-
 ### sound effects
 
-# select an alert sound, e.g. "Sosumi"
-#defaults write NSGlobalDomain com.apple.sound.beep.sound -string "/System/Library/Sounds/Sosumi.aiff"
+# alert sound
+# select an alert sound and check name with 
+# defaults read NSGlobalDomain com.apple.sound.beep.sound
+defaults write NSGlobalDomain com.apple.sound.beep.sound -string "/System/Library/Sounds/Bottle.aiff"
 
-# play sound on startup
-# %00 = on
-# %01 = off
-sudo nvram StartupMute=%01
+# play sound effects through (default: selected sound output device)
+# TODO
 
-# play user interface sound effects
-# 1 = yes, 0 = no
+# alert volume (default: 1)
+defaults write NSGlobalDomain com.apple.sound.beep.volume -float 1
+
+# play sound on startup (default: on)
+# on = %00
+# off = %01
+sudo nvram StartupMute=%00
+
+# play user interface sound effects (default: on)
+# on = 1
+# off = 0
 defaults write NSGlobalDomain com.apple.sound.uiaudio.enabled -int 1
 
-# play feedback when volume is changed
-# 1 = yes, 0 = no
-defaults write NSGlobalDomain com.apple.sound.beep.feedback -int 1
+# play feedback when volume is changed (default: off)
+# on = 1
+# off = 0
+defaults write NSGlobalDomain com.apple.sound.beep.feedback -int 0
+
 
 ### output & input
 
+# output volume
+# TODO
+
+# mute
+# TODO
+
+# balance
+# TODO
 
 
 ### hidden sound tweaks
 
-# increase sound quality for Bluetooth headphones/headsets
-# requires higher bandwith, might have a negative effect on stability of the Bluetooth connection
+# settings that are commented out are out of date or couldn't be validated to see if they were out of date or not
+
+# increase sound quality for bluetooth headphones/headsets
+# requires higher bandwith, might have a negative effect on stability of the bluetooth connection
 # defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
