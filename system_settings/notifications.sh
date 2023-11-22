7c1b97f1c517fe81f4fa5452679451e15138c05b
Philip Reinhart <74872363+philiprein@users.noreply.github.com>
2023-11-22 15:11:17 +0100

    update notification settings

--- system_settings/notifications.sh
+++ system_settings/notifications.sh
@@ -2,22 +2,23 @@
 
 echo "Notifications settings..."
 
-# show previews
+# show previews (default: 2)
 # 1 = never
 # 2 = if unlocked
 # 3 = always
-defaults write /Users/"$USER"/Library/Preferences/com.apple.ncprefs.plist content_visibility -int 2
+defaults write com.apple.ncprefs content_visibility -int 2
 
-# do not allow notifications when the display is sleeping
-# allow notifications when the screen is locked
-# allow notifications when mirroring or sharing the display
+# allow notifications when the display is sleeping (default: off)
+# allow notifications when the screen is locked (default: on)
+# allow notifications when mirroring or sharing the display (default: off)
+# false = true in GUI
 DND_HEX_DATA=$(echo '
   <?xml version="1.0" encoding="UTF-8"?>
   <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
   <plist version="1.0">
   <dict>
     <key>dndDisplayLock</key>
-    <false/> 
+    <false/>
     <key>dndDisplaySleep</key>
     <true/>
     <key>dndMirrored</key>
@@ -29,17 +30,23 @@ DND_HEX_DATA=$(echo '
   </dict>
   </plist>' - -o - | plutil -convert binary1 - -o - | xxd -p | tr -d '\n')
 
-defaults write com.apple.ncprefs.plist dnd_prefs -data "$DND_HEX_DATA"
-killall cfprefsd; while [[ $(ps aux | grep cfprefsd | grep -v grep | awk '{print $2;}') == "" ]]; do sleep 0.5; done
-killall usernoted; while [[ $(ps aux | grep usernoted | grep -v grep | awk '{print $2;}') == "" ]]; do sleep 0.5; done
+defaults write com.apple.ncprefs dnd_prefs -data "$DND_HEX_DATA"
 
-### per app notification settings
-# see seperate script
+# for changes to take effect
+killall cfprefsd
+while [[ $(ps aux | grep cfprefsd | grep -v grep | awk '{print $2;}') == "" ]]; do sleep 0.5; done
+killall usernoted
+while [[ $(ps aux | grep usernoted | grep -v grep | awk '{print $2;}') == "" ]]; do sleep 0.5; done
+
+
+### application notifications
+# TODO
+
 
 ### hidden notification center tweaks
-    
-# change notification banner persistence time (value in seconds)
-# defaults write com.apple.notificationcenterui bannerTime 15
 
-# reset default notification banner persistence time
-# defaults delete com.apple.notificationcenterui bannerTime
+# settings that are commented out are out of date or couldn't be validated to see if they were out of date or not
+
+# change notification banner persistence time (value in seconds)
+# doesn't seem to work anymore
+# defaults write com.apple.notificationcenterui bannerTime 15
