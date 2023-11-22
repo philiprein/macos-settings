a8debeb81093d1a8ac364155cd81001d7b554fdf
Philip Reinhart <74872363+philiprein@users.noreply.github.com>
2023-11-22 15:10:51 +0100

    update general settings

--- system_settings/general.sh
+++ system_settings/general.sh
@@ -10,51 +10,70 @@ echo "General settings..."
 
 # to enable "Automatic updates" set all options below to true
 
-# check for updates automatically
-sudo softwareupdate --schedule on
-# or
-#sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+# check for updates (default: on)
+sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
 
-# download new updates in the background when available
+# download new updates when available (default: on)
 sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticDownload -bool true
 
-# install macos updates automatically
+# install macos updates automatically (default: on)
 sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate AutomaticallyInstallMacOSUpdates -bool false
 
-# install application updates from the App Store automatically
+# install application updates from the App Store automatically (default: on)
 sudo defaults write /Library/Preferences/com.apple.commerce AutoUpdate -bool true
 
-# install system and security updates automatically
+# install system and security updates automatically (default: on)
 sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate ConfigDataInstall -bool true
 sudo defaults write /Library/Preferences/com.apple.SoftwareUpdate CriticalUpdateInstall -bool true
 
 
 ### storage
 
+# store in icloud
+# TODO
+
+# optimize storage (default: off)
+defaults write com.apple.TV automaticallyDeleteVideoAssetsAfterWatching -bool false
+
+# empty bin automatically (default: off)
+defaults write com.apple.finder FXRemoveOldTrashItems -bool false
 
 
-### airdrop and handoff
+### airdrop & handoff
 
-# allow handoff between this mac and your icloud devices
-# false = off, true = on
+# allow handoff between this mac and your icloud devices (default: on)
+# on = true, true
+# off = false, false
 defaults -currentHost write com.apple.coreservices.useractivityd ActivityAdvertisingAllowed -bool true
 defaults -currentHost write com.apple.coreservices.useractivityd ActivityReceivingAllowed -bool true
 
-# set airdrop discoverable mode 
-# Off 
-# Contacts Only
-# Everyone
+# airdrop (default: "Contacts Only")
+# no one = "Off"
+# contacts only = "Contacts Only"
+# everyone = "Everyone"
 defaults write com.apple.sharingd DiscoverableMode -string "Contacts Only"
+
+# airplay receiver (default: on)
+defaults write -currentHost com.apple.controlcenter AirplayRecieverEnabled -bool true
+    
+# allow airplay for (default: 1)
+# current user = 1
+# anyone on the same network = 2
+# everyone = 3
+defaults write -currentHost com.apple.controlcenter AirplayReceiverAdvertising -int 1
+
+# require password
+# TODO
 
 
 ### login items
 
 # open at login
-
+# TODO
 # apps generate the open at login entry automatically when they are first launched
 
 # allow in the background
-
+# TODO
 # it seems an entry in the BackgroundItems file is needed
 # readable output
 # sfltool dumpbtm /private/var/db/com.apple.backgroundtaskmanagement/BackgroundItems-v8.btm
@@ -74,105 +93,116 @@ defaults write com.apple.sharingd DiscoverableMode -string "Contacts Only"
 
 ### language & region
 
-# setup assistant language (system and login screen)
-# default: en
-#sudo languagesetup -langspec en
-
-# preferred languages
+# preferred languages & region (default: set in setup assistant)
 defaults write NSGlobalDomain AppleLanguages -array "en-DE" "de-DE"
 defaults write NSGlobalDomain AppleLocale -string "en_DE@currency=EUR"
 
-# calendar & first day of week
-# 2 = monday, 3 = tuesday, ...
+# calendar & first day of week  (default: set in setup assistant)
+# sunday = 1
+# monday = 2 
+# ...
+# saturday = 6
 defaults write NSGlobalDomain AppleFirstWeekday -array "gregorian = 2"
 
-# temperature
+# temperature  (default: set in setup assistant)
 defaults write NSGlobalDomain AppleTemperatureUnit -string "Celsius"
 
-# measurement system
+# measurement system  (default: set in setup assistant)
 defaults write NSGlobalDomain AppleMeasurementUnits -string "Centimeters"
 defaults write NSGlobalDomain AppleMetricUnits -bool true
 
-# list sort order (default: universal)
+# list sort order (default: set in setup assistant)
 defaults write NSGlobalDomain AppleCollationOrder -string "de@collation=universal"
-# or
-#defaults delete NSGlobalDomain AppleCollationOrder
 
-# live text
+# term of address
+# "", masculine, feminine, neuter
+defaults write com.apple.morphology.internal AppleUserMorphology -dict-add grammaticalGender masculine
+
+# share with all applications
+defaults write com.apple.morphology.internal AppleUserMorphologyAllowThirdPartyAccess -bool false
+
+# live text (default: on)
 defaults write NSGlobalDomain AppleLiveTextEnabled -bool true
+
+# translation languages...
+# TODO
 
 
 ### date & time
 
-# set date and time automatically
+# set date and time automatically (default: on)
 sudo systemsetup -setusingnetworktime on
 
-# source
+# source (default: sudo systemsetup -getnetworktimeserver)
+sudo systemsetup -setnetworktimeserver time.apple.com &>/dev/null
 
-# get default
-# sudo systemsetup -getnetworktimeserver
-
-# use default by not setting anything else
-# redirecting output due to this error message
-# Error:-99 File:/AppleInternal/Library/BuildRoots/0783246a-4091-11ee-8fca-aead88ae2785/Library/Caches/com.apple.xbs/Sources/Admin/InternetServices.m Line:379
-# sudo systemsetup -setnetworktimeserver time.apple.com 2>/dev/null 1>&2
-
-# 12 hour clock
-# 12h clock off = 24h clock on
+# 12-hour time (default: set in setup assistant)
 # be sure the system settings window is not open when using this or it won`t work
-# defaults write NSGlobalDomain AppleICUForce12HourTime -bool false
-# will be set to AppleICUForce24HourTime later
-# defaults delete NSGlobalDomain AppleICUForce12HourTime
+# 12-hour time
+defaults write NSGlobalDomain AppleICUForce12HourTime -bool true
+# 24-hour time
+defaults write NSGlobalDomain AppleICUForce24HourTime -bool false
 
-# set time zone automatically using current location
-sudo defaults write /Library/Preferences/com.apple.timezone.auto.plist Active -bool true
+# set time zone automatically using your current location (default: on)
+# TODO
+# seems to not work anymore...
+# sudo defaults write /Library/Preferences/com.apple.timezone.auto.plist Active -bool true
 
-# set the timezone; see "systemsetup -listtimezones" for other values
-# redirecting output due to this error message
-# Error:-99 File:/AppleInternal/Library/BuildRoots/0783246a-4091-11ee-8fca-aead88ae2785/Library/Caches/com.apple.xbs/Sources/Admin/InternetServices.m Line:379
-sudo systemsetup -settimezone "Europe/Berlin" 2>/dev/null 1>&2
+# closest city
+# see "systemsetup -listtimezones" for other values
+sudo systemsetup -settimezone "Europe/Berlin" &>/dev/null
 
 
 ### sharing
 
-# screen sharing
-
-# allow to ask for permission to share screen
-# sudo defaults write /Library/Preferences/com.apple.RemoteManagement.plist ScreenSharingReqPermEnabled -bool true
-
-# file sharing
-
-# deactivate smb file server
-# if [[ $(sudo launchctl list | grep com.apple.smbd) == "" ]] &>/dev/null; then
-#   : 
-# else
-#   sudo launchctl bootout system "/System/Library/LaunchDaemons/com.apple.smbd.plist" 2>&1 | grep -v "in progress" | grep -v "No such process"
-#   sleep 2
-#   sudo launchctl disable system/com.apple.smbd
-# fi
-
-# deactivate afp file server
-# if [[ $(sudo launchctl list | grep com.apple.AppleFileServer) == "" ]] &>/dev/null; then
+# enabling seems to require entries in write protected kTCC database
+# enable_sharing_service() {
+#   if [[ $(sudo launchctl list | grep "$1") == "" ]] &>/dev/null; then
+#     sudo launchctl enable system/"$1"
+#     sleep 2
+#     sudo launchctl bootstrap system "/System/Library/LaunchDaemons/"$1".plist" 2>&1 | grep -v "in progress" | grep -v "already bootstrapped"
+#   else
 #     :
-# else
-#   sudo launchctl bootout system "/System/Library/LaunchDaemons/com.apple.AppleFileServer.plist" 2>&1 | grep -v "in progress" | grep -v "No such process"
-#   sleep 2
-#   sudo launchctl disable system/com.apple.AppleFileServer
-# fi
+#   fi
+# }
 
-# turn off internet sharing
-# if [[ $(sudo launchctl list | grep com.apple.InternetSharing) == "" ]] &>/dev/null; then
-#   :
-# else
-#   sudo launchctl bootout system "/System/Library/LaunchDaemons/com.apple.InternetSharing.plist" 2>&1 | grep -v "in progress" | grep -v "No such process"
-#   sleep 2
-#   sudo launchctl disable system/com.apple.InternetSharing
-# fi
+# disable_sharing_service() {
+#   if [[ $(sudo launchctl list | grep "$1") == "" ]] &>/dev/null; then
+#     :
+#   else
+#     sudo launchctl bootout system "/System/Library/LaunchDaemons/"$1".plist" 2>&1 | grep -v "in progress" | grep -v "No such process"
+#     sleep 2
+#     sudo launchctl disable system/"$1"
+#   fi
+# }
 
-# printer sharing
 
-# cupsctl --share-printers
-# cupsctl --no-share-printers
+# screen sharing (default: off)
+# TODO
+# disable_sharing_service "com.apple.screensharing"
+
+# allow access for
+# TODO
+
+# anyone may request permission to control screen
+sudo defaults write /Library/Preferences/com.apple.RemoteManagement.plist ScreenSharingReqPermEnabled -bool true
+
+# VNC viewers may control screen with password
+# TODO
+
+
+# file sharing (default: off)
+# TODO
+# deactivate smb file server
+# disable_sharing_service "com.apple.smbd"
+# deactivate afp file server
+# disable_sharing_service "com.apple.AppleFileServer"
+
+
+# printer sharing (default: off)
+# on = --share-printers
+# off = --no-share-printers
+cupsctl --no-share-printers
 # sets preferences in 
 # /etc/cups/cupsd.conf
 # /etc/cups/printers.conf
@@ -180,60 +210,49 @@ sudo systemsetup -settimezone "Europe/Berlin" 2>/dev/null 1>&2
 # system_profiler SPPrintersDataType | grep "Printer Sharing"
 # system_profiler SPPrintersDataType | grep Shared
 
-# remote login
 
-# sudo systemsetup -setremotelogin on
-# sudo systemsetup -setremotelogin off
+# remote login (default: off)
+# on = on
+# off = off
+sudo systemsetup -setremotelogin off
 # check
 # sudo systemsetup -getremotelogin
 
-# remote management
 
-# screen sharing has to be enabled additionally for this to work
-# sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
-# sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -deactivate -stop
+# remote management (default: off)
+# TODO
 
-# remote apple events
 
-# sudo systemsetup -setremoteappleevents on
-# sudo systemsetup -setremoteappleevents off
+# remote apple events (default: off)
+# on = on
+# off = off
+sudo systemsetup -setremoteappleevents off
 # check
 # sudo systemsetup -getremoteappleevents | grep "Apple Events"
 
-# internet sharing
 
+# internet sharing (default: off)
+# TODO
+# disable_sharing_service "com.apple.InternetSharing"
 # disable
 # sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.nat NAT -dict Enabled -int 0
 # check
 # sudo defaults read /Library/Preferences/SystemConfiguration/com.apple.nat | grep -i Enabled
 
-# content caching
 
-# sudo AssetCacheManagerUtil activate
-# sudo AssetCacheManagerUtil deactivate
+# content caching (default: off)
+# on = activate
+# off = deactivate
+sudo AssetCacheManagerUtil deactivate
 
-# media sharing (needs reboot)
 
-# defaults write com.apple.amp.mediasharingd public-sharing-enabled -bool false
-# defaults write com.apple.amp.mediasharingd home-sharing-enabled -bool false
+# media sharing (default: off) (needs reboot)
+defaults write com.apple.amp.mediasharingd public-sharing-enabled -bool false
+defaults write com.apple.amp.mediasharingd home-sharing-enabled -bool false
 
-# bluetooth sharing
 
-# needs logout / reboot
-# defaults -currentHost write com.apple.bluetooth PrefKeyServicesEnabled -bool true
-# defaults -currentHost write com.apple.bluetooth PrefKeyServicesEnabled -bool false
-    
-# airplay receiver
-
-# enable/disable
-# defaults -currentHost write com.apple.controlcenter AirplayRecieverEnabled -bool true
-# defaults -currentHost write com.apple.controlcenter AirplayRecieverEnabled -bool false
-# if enabled
-# 1 = allow from same icloud user
-# 2 = allow from complete same network
-# 3 = allow from everyone
-# defaults -currentHost write com.apple.controlcenter AirplayReceiverAdvertising -int 1
-
+# bluetooth sharing (default: off) (needs logout)
+defaults -currentHost write com.apple.bluetooth PrefKeyServicesEnabled -bool false
 
 
 # local hostname
@@ -249,34 +268,50 @@ MY_HOSTNAME=$(system_profiler SPHardwareDataType |
 sudo scutil --set ComputerName "$MY_HOSTNAME"
 sudo scutil --set LocalHostName "$MY_HOSTNAME"
 sudo scutil --set HostName "$MY_HOSTNAME"
-#sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$MY_HOSTNAME"
+# not needed, gets set automatically
+# sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "$MY_HOSTNAME"
 dscacheutil -flushcache
 unset MY_HOSTNAME
+
+# use dynamic global hostname
+# TODO
 
 
 ### time machine
 
-# disable time machine
-sudo defaults write /Library/Preferences/com.apple.TimeMachine MobileBackups -bool false
-sudo defaults write /Library/Preferences/com.apple.TimeMachine AutoBackup -bool false
+# back up frequency
+# manually
+# sudo defaults delete /Library/Preferences/com.apple.TimeMachine AutoBackupInterval
+# automatically every hour = 3600
+# automatically every day = 86400
+# automatically every week = 604800
+sudo defaults write /Library/Preferences/com.apple.TimeMachine AutoBackupInterval -int 3600
 
-# run time machine backups on battery power
+# back up on battery power
 sudo defaults write /Library/Preferences/com.apple.TimeMachine RequiresACPower -bool true
 
+
+### hidden time machine tweaks
+
+# settings that are commented out are out of date or couldn't be validated to see if they were out of date or not
+
+# disable/enable time machine
+sudo defaults write /Library/Preferences/com.apple.TimeMachine MobileBackups -bool true
+sudo defaults write /Library/Preferences/com.apple.TimeMachine AutoBackup -bool true
+
 # exclude system files
-sudo defaults write /Library/Preferences/com.apple.TimeMachine SkipSystemFiles -bool true
+# sudo defaults write /Library/Preferences/com.apple.TimeMachine SkipSystemFiles -bool true
 
 # show warning after deleting old backups
-sudo defaults write /Library/Preferences/com.apple.TimeMachine AlwaysShowDeletedBackupsWarning -bool true
+# sudo defaults write /Library/Preferences/com.apple.TimeMachine AlwaysShowDeletedBackupsWarning -bool true
     
 # prevent time machine from prompting to use new hard drives as backup volume
-defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+# defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 # disable local time machine backups / snapshots
 # sudo tmutil disable
 
 # delete possible snapshots
-
 # list localsnapshots
 # tmutil listlocalsnapshots / | cut -d'.' -f4-
 # tmutil listlocalsnapshots / | rev | cut -d'.' -f1 | rev
@@ -302,3 +337,10 @@ defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 ### startup disk
 
 
+
+### hidden general tweaks
+
+# settings that are commented out are out of date or couldn't be validated to see if they were out of date or not
+
+# setup assistant language (system and login screen)
+# sudo languagesetup -langspec en
